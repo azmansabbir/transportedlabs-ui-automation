5a49a6fd607e1d98cef0d958d006ab825d559bce
azmansabbir <asabbir@transportedlabs.com>
2022-09-05 17:56:55 +0600

    Updated Login Test

--- test/pageObjectTest/TC002.test.ts
+++ test/pageObjectTest/TC002.test.ts
@@ -1,72 +1,127 @@
-import CommonFunctions from "../../page/common.page";
-import HeaderPage from "../../page/Header.page";
 import LoginPage from "../../page/Login.page";
 import Env from "../../utils/environment";
 import * as data from "../../data/login.cred.json";
 import { Page } from "playwright";
 import ReportUtils from "../../utils/reportUtils";
 
+
 declare const page: Page;
 declare const reporter: any;
 
-describe("TC002", () => {
+describe("TL001", () => {
     // my pages
-    let header: HeaderPage;
     let login: LoginPage;
-    let common: CommonFunctions;
 
     beforeAll(async () => {
         await page.goto(Env.test);
-        header = new HeaderPage(page);
         login = new LoginPage(page);
-        common = new CommonFunctions(page);
+
     })
 
-    afterAll(async () => {
-        await page.close();
-        await context.close();
-        await browser.close();
-    })
 
-    test("TC002 Login 1", async () => {
+    test("Try to login with invalid Credentials", async () => {        
+
         await reporter
-            .description("Login into LetCode")
-            .story("JIRA101");
-        await reporter.startStep("Navigate to letcode");
-        expect(page.url()).toBe("https://letcode.in/")
-        await ReportUtils.screenshot("naviagation")
-        await reporter.endStep();
-        await reporter.startStep("Click login link");
-        await header.clickLoginLink();
-        await reporter.endStep();
-        expect(page.url()).toBe("https://letcode.in/signin")
-        await reporter.startStep("enter username");
-        await login.enterUserName(data.username);
-        await ReportUtils.screenshot("username")
-        // const screenshotBuffer = await page.screenshot();
-        // await reporter.addAttachment("username", screenshotBuffer, "image/png");
-        await reporter.endStep();
-        await reporter.startStep("enter password");
-        await login.enterUserPassword(data.password);
-        await ReportUtils.screenshot();
-        await reporter.endStep();
-        await login.clickLoginBtn();
-        const toaster = await common.toaster();
-        expect(await toaster?.textContent()).toContain("Welcome");
-        await reporter.startStep("Log out");
-        await header.clickSignOutLink();
-        await ReportUtils.screenshot("done")
+        .description("Login into TransportedLabs with valid credentials")
+        .story("Task001");
+
+        await reporter.startStep("Navigate to the url");
+        await page.goto(Env.test, {
+            waitUntil: "domcontentloaded"
+        });      
+        await ReportUtils.screenshot("Navigate to the url")
         await reporter.endStep();
 
-    });
-    // test("TC002 Login 2", async () => {
-    //     await page.goto(Env.test, {
-    //         waitUntil: "domcontentloaded"
-    //     });
-    //     await header.clickLoginLink();
-    //     await login.login("koushik350@gmail.com", "Pass123$");
-    //     await page.waitForNavigation();
-    //     expect(page.url()).toBe("https://letcode.in/")
-    //     await ReportUtils.screenshot("done")
-    // })
+
+        await reporter.startStep("Enter username");
+        await page.locator('input[type="text"]').click(); // Click to input id
+        await page.locator('input[type="text"]').fill(data.invalidusername); // Fill id
+        await ReportUtils.screenshot("username")
+        await reporter.endStep();
+
+        await reporter.startStep("Enter Password");    
+        await page.locator('input[type="password"]').click(); // Click to input password
+        await page.locator('input[type="password"]').fill(data.invalidpassword); // Fill incorrect password
+        await ReportUtils.screenshot("Enter Password")
+        await reporter.endStep();
+
+        await reporter.startStep("Click on the login button");  
+        await page.locator('button:has-text("Login")').click(); // Click "Login"
+        await ReportUtils.screenshot("Click on the login button")
+        await reporter.endStep();
+
+
+        await reporter.startStep("Invalid credentials");    
+        expect(page.locator('p:has-text("Invalid credentials")'))
+        await ReportUtils.screenshot("Invalid credentials")
+        await reporter.endStep();
+
+        await reporter.startStep("Click on the ok button");  
+        await page.locator('button:has-text("Ok")').click(); // Click Ok
+        await ReportUtils.screenshot("Click on the ok button")
+        await reporter.endStep();
+
+        await reporter.startStep("Should be show login page"); 
+        expect(page.url()).toContain(Env.test); 
+        await ReportUtils.screenshot("Should be show login page")
+        await reporter.endStep();       
+
+      
+    })
+
+    test("Try to login with valid Credentials", async () => {        
+
+        await reporter
+        .description("Login into TransportedLabs with valid credentials")
+        .story("Task002");
+
+        await reporter.startStep("Navigate to the url");
+        await page.goto(Env.test, {
+            waitUntil: "domcontentloaded"
+        });      
+        await ReportUtils.screenshot("Navigate to the url")
+        await reporter.endStep();
+
+
+
+        await reporter.startStep("Enter username");
+        await page.locator('input[type="text"]').click(); // Click to input id
+        await page.locator('input[type="text"]').fill(data.username); // Fill id
+        await ReportUtils.screenshot("username")
+        await reporter.endStep();
+
+        await reporter.startStep("Enter Password");    
+        await page.locator('input[type="password"]').click(); // Click to input password
+        await page.locator('input[type="password"]').fill(data.password); // Fill incorrect password
+        await ReportUtils.screenshot("Enter Password")
+        await reporter.endStep();
+
+        await reporter.startStep("Click on the login button");  
+        await page.locator('button:has-text("Login")').click(); // Click "Login"
+        await ReportUtils.screenshot("Click on the login button")
+        await reporter.endStep();
+
+        await reporter.startStep("Should be show home page"); 
+        expect(page.url()).toContain(Env.test); 
+        await ReportUtils.screenshot("Should be show home page")
+        await reporter.endStep();       
+
+      
+    })
+
+    xtest("Login New", async () => {
+        await page.goto(Env.test, {
+            waitUntil: "domcontentloaded"
+        });
+
+
+
+        await login.enterUserName(data.username);
+
+        await login.enterUserPassword(data.password);
+
+        await login.clickLoginBtn()
+        
+      
+    })
 })
